606366379ea4cae831d47c96d6ff8c07146b7df3
Yelzhan Yerkebulan <33483071+030131550542@users.noreply.github.com>
2020-03-16 02:07:04 +0600

    Update firebase.js

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -3,15 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions';
 
-const config = {
-    apiKey: "AIzaSyDr9IQ7YP0aVPDf7hRfNgwo0YlOB4SqvlI",
-    authDomain: "qazcoronabot-spmwxp.firebaseapp.com",
-    databaseURL: "https://qazcoronabot-spmwxp.firebaseio.com",
-    projectId: "qazcoronabot-spmwxp",
-    storageBucket: "qazcoronabot-spmwxp.appspot.com",
-    messagingSenderId: "1082693256911",
-    appId: "1:1082693256911:web:2f843297f9f0d597bd19ba"
-  };
+const config = {<CONFIG_DATA>};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
